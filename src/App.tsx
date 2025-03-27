fbdaba3e430fa60add4f8b18f9fd62f6c97375e3
Isaque Dias <isaque.brain@gmail.com>
2025-03-27 11:04:49 -0300

    input field adicionado, task stats iniciado

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <header className="banner-background">
-        <img src="src\assets\Logo.svg" alt="logo do todo" className="logo" />
-      </header>
-
+      <body className="container">
+          <header className="banner-background">
+            <img
+              src="src\assets\Logo.svg"
+              alt="logo do todo"
+              className="logo"
+            />
+          </header>
+          <div className="input-field">
+            <div className="input-shape">
+              <input type="text" placeholder="Adicione uma nova tarefa" />
+            </div>
+            <button>Criar</button>
+          </div>
+          <div className="stats-container">
+            <div className="stats">
+              <div className="created">
+                <p>
+                  Tarefas criadas<span className="counter">{0}</span>
+                </p>
+              </div>
+              <div className="completed">
+                <p>
+                  Concluídas<span className="counter">{0}</span>
+                </p>
+              </div>
+            </div>
+          </div>
+      </body>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
